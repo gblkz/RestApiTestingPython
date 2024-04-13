752c4b0b67365282f3b46bd970770ca436920be2
Gordon Lippa <gordon.lippa@gmail.com>
2024-04-12 21:58:00 -0700

    Edit to project demo section.

--- PyTest/pixegami/test_todo_api.py
+++ PyTest/pixegami/test_todo_api.py
@@ -19,7 +19,7 @@ import uuid
 
 ENDPOINT = "https://todo.pixegami.io"
 """
-# Initial demo of principles.
+# Initial demo of principles. Review this video, then maybe move this to an external doc.
 response = requests.get(ENDPOINT)   # Making an HTTP Request.
 print(response)                     # prints just the status code, [200].
 data = response.json()              # returns the entire response message.
