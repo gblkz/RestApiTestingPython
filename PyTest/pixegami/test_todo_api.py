4f2118a0775409359b91386dafaf07a97989140c
Gordon Lippa <gordon.lippa@gmail.com>
2024-04-18 20:39:44 -0700

    Added docker options for Jira deployment.

--- PyTest/pixegami/test_todo_api.py
+++ PyTest/pixegami/test_todo_api.py
@@ -12,7 +12,7 @@ Deploy a defect DB for code review. Need to accommodate all project types:
 - Guilded Rose refactor kata. Bug initial version as review finds.
 - Book projects from "Python Crash Course".
 - freeCodeCamp backend projects. And Derek Banas projects.
-
+- Docker options?
 
 """
 import requests
