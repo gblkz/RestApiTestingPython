a4e242a152da7b2104afbfb50df1ae1ddf0e43a7
Gordon Lippa <gordon.lippa@gmail.com>
2024-05-16 15:10:24 -0700

    Removed duplicate spec.

--- PyTest/pixegami/test_todo_api.py
+++ PyTest/pixegami/test_todo_api.py
@@ -13,8 +13,7 @@ Deploy a defect DB for code review. Need to accommodate all project types (won't
 - Docker options for deploying Jira (freeCodeCamp).
 - Backend projects (freeCodeCamp, Banas).
 - Sys Design projects.
-- Automation backlog from manual tests.
-- Where does TCM occur?
+- Automation backlog from manual tests. Where does TCM occur?
 
 """
 import requests
