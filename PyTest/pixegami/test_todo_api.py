7c8b17c0ea51afdbc68c8cce0bbfc4a0ec3ad24b
Gordon Lippa <gordon.lippa@gmail.com>
2024-05-20 14:53:14 -0700

    Addd option for Windows Scheduler before CICD.

--- PyTest/pixegami/test_todo_api.py
+++ PyTest/pixegami/test_todo_api.py
@@ -14,7 +14,7 @@ Deploy a defect DB for code review. Need to accommodate all project types (won't
 - Backend projects (freeCodeCamp, Banas).
 - Sys Design projects.
 - Automation backlog from manual tests. Where does TCM occur?
-- How do we schedule automated test suites? CICD?
+- How do we schedule automated test suites? CICD? Maybe for now just use Windows Scheduler.
 
 """
 import requests
