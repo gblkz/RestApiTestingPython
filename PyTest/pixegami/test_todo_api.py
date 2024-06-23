b4752100b4e2b20b85b8bea059a75c8ed2ec8b4c
Gordon Lippa <gordon.lippa@gmail.com>
2024-06-22 22:00:45 -0700

    Added test endpoint variable.

--- PyTest/pixegami/test_todo_api.py
+++ PyTest/pixegami/test_todo_api.py
@@ -23,6 +23,7 @@ import requests
 import uuid
 
 ENDPOINT = "https://todo.pixegami.io"
+# ENDPOINT = "https://todo.pixegami.io"
 """
 # Initial demo of principles. Review concepts, don't memorize, then move section to external doc.
 response = requests.get(ENDPOINT)   # Making an HTTP Request.
