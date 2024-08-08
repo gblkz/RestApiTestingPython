6edf8f0b4f04eb87d7c338f2ba029589d19b0bee
Gordon Lippa <gordon.lippa@gmail.com>
2024-08-07 21:36:21 -0700

    Added new test endpoint.

--- PyTest/pixegami/test_todo_api.py
+++ PyTest/pixegami/test_todo_api.py
@@ -25,7 +25,7 @@ import requests
 import uuid
 
 ENDPOINT = "https://todo.pixegami.io"
-
+ENDPOINT_TEST = "https://www.google.com"
 """
 # Initial demo of principles. Review concepts, don't memorize implementation.
 response = requests.get(ENDPOINT)   # HTTP Request, dummy.
